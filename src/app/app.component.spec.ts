4401ffe659ef025b27c2c979b03333161b102591
rainerpons <rainerpons11@gmail.com>
2022-10-09 13:45:47 -0700

    Changed readonly variable to uppercase

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'config-stash'`, () => {
+  it(`should have as title 'Config Stash'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('config-stash');
+    expect(app.TITLE).toEqual('Config Stash');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('config-stash app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Config Stash app is running!');
   });
 });
