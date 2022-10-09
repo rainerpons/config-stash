85eed58cf3c3b09854a6c0ee8c9d3648d40573b3
rainerpons <rainerpons11@gmail.com>
2022-10-09 16:46:10 -0700

    Implement OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Config } from './models/config';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   readonly TITLE = 'Config Stash';
+  configs: Config[] = [];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    // 1. Initialize connection to database
+    this.dataService.initialize();
+
+    // 2. Grab all data and store as member variable
+    this.configs = this.dataService.getConfigs();
+    console.log('configs:', this.configs);
+  }
 }
