45ad418bd201b20f1ab7ac33f67815ad84ba1e3d
rainerpons <rainerpons11@gmail.com>
2022-10-09 16:44:05 -0700

    Created service class for retrieving data (not implemented yet)

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,12 @@
+import { Config } from '../models/config';
+
+export class DataService {
+    initialize(): void {
+        // TODO: Implement
+    }
+
+    getConfigs(): Config[] {
+        // TODO: Implement
+        return [];
+    }
+}
